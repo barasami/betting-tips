155868d44eedabb5c575b0c697cafe8de1502b22
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-06-15 22:22:23 +0300

    added mui

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { coolTips } from './tips'
 import Form from './Form'
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 function Home() {
     const[tip,setTips]=useState([])
+    const[load,setLoad]=useState(false)
     const[mytip,setMytip]=useState('2023-5-20')
     useEffect(()=>{
+        setLoad(true)
         coolTips(mytip)
         .then(({data})=>{
             setTips(data)
+            setLoad(false)
         })
     },[mytip])
 
@@ -18,10 +23,37 @@ function Home() {
 
     let myTips=tip?.data
     console.log(myTips);
+
+    let tipsResult=myTips?.map((datas)=>{
+        const{home_team,away_team,id,prediction,result,competition_cluster}=datas
+        return(
+            <div key={id}>
+                <div>
+                    <table>
+                        <tr>
+                            <td>{home_team}</td>
+                            <td>{away_team}</td>
+                            <td>{prediction}</td>
+                            <td>{result}</td>
+                            <td>{competition_cluster}</td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
+        )
+    })
   return (
     <div>
         <div>
             <Form findTip={findTip}/>
+        </div>
+        <div>
+            {load ? <CircularProgress color='inherit' className='circular'/> : 
+            <div>
+                {tipsResult}
+            </div>
+            }
         </div>
     </div>
   )
